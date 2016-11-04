6ae28d6f15818c0f7dc1a792b01d26b6e4979f8c
Nie <1063086740@qq.com>
2016-11-04 13:39:42 +0800

    DEV:商务合作

---
+++ QuickPay/Classes/Sections/PersonalCenter/View/QPBusinessCooperationTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  QPBusinessCooperationTableViewCell.h
+//  QuickPay
+//
+//  Created by Nie on 2016/11/4.
+//  Copyright © 2016年 Nie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QPBusinessCooperationTableViewCell : UITableViewCell
+
+@end
---
+++ QuickPay/Classes/Sections/PersonalCenter/View/QPBusinessCooperationTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  QPBusinessCooperationTableViewCell.m
+//  QuickPay
+//
+//  Created by Nie on 2016/11/4.
+//  Copyright © 2016年 Nie. All rights reserved.
+//
+
+#import "QPBusinessCooperationTableViewCell.h"
+
+@implementation QPBusinessCooperationTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
