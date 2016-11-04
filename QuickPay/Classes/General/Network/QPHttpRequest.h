139476ba0f0f517606aed4c40527f8d4c28d9107
Nie <1063086740@qq.com>
2016-11-04 13:45:50 +0800

    DEV:

---
+++ QuickPay/Classes/General/Network/QPHttpRequest.h
@@ -0,0 +1,13 @@
+//
+//  QPHttpRequest.h
+//  QuickPay
+//
+//  Created by Nie on 2016/11/4.
+//  Copyright © 2016年 Nie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QPHttpRequest : NSObject
+
+@end
---
+++ QuickPay/Classes/General/Network/QPHttpRequest.m
@@ -0,0 +1,13 @@
+//
+//  QPHttpRequest.m
+//  QuickPay
+//
+//  Created by Nie on 2016/11/4.
+//  Copyright © 2016年 Nie. All rights reserved.
+//
+
+#import "QPHttpRequest.h"
+
+@implementation QPHttpRequest
+
+@end
